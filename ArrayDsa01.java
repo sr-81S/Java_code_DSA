ec8c13c4a102c390ee36f9d8ee53958544885a41
saroz savar <sarojsabar12345@gmail.com>
2022-09-04 22:21:50 +0530

    first java DSA code

---
+++ ArrayDsa01.java
@@ -0,0 +1,50 @@
+package DSA_java;
+
+import java.util.Scanner;
+
+
+
+
+
+public class ArrayDsa01 {
+
+    //print array function
+    static void printArray(int arr[]){
+        for (int x = 0;x<arr.length;x++){
+            System.out.print(arr[x]+" ");
+        }
+    }
+
+    //reverse array function
+
+    static void reverseArray(int arr[]){
+        int start =0;
+        int end = arr.length -1;
+        int tem ;
+        while(start<end){
+            tem = arr[start];
+            arr[start] = arr[end];
+            arr[end]=tem;
+            start++;
+            end--;
+        }
+        System.out.println();
+        printArray(arr);
+    }
+
+
+
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int ind = input.nextInt();
+        int arr[] = new int[ind];
+
+        for (int i=0;i<=ind-1;i++) {
+            arr[i] = input.nextInt();
+        }
+        printArray(arr);
+        System.out.println();
+        reverseArray(arr);
+    }
+}
